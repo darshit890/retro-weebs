a64446bf4d3eb50b9d47d44eae6af59861f92ec9
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-09-18 11:54:33 +0530

    Update categories.ts

--- lib/categories.ts
+++ lib/categories.ts
@@ -1,27 +1,29 @@
+import { Category } from "@prisma/client";
+
 export const categories = [
-    {
-      id: 0,
-      title: "T-shirts",
-      name: "tshirts",
-    },
-    {
-      id: 1,
-      title: "Oversized tees",
-      name: "oversized",
-    },
-    {
-      id: 3,
-      title: "Joggers",
-      name: "joggers",
-    },
-    {
-      id: 4,
-      title: "Hoodies",
-      name: "hoodies",
-    },
-    {
-      id: 5,
-      title: "Shorts",
-      name: "shorts",
-    },
-  ];
+  {
+    id: 0,
+    title: "T-shirts",
+    name: "tshirts" as Category,
+  },
+  {
+    id: 1,
+    title: "Oversized tees",
+    name: "oversized" as Category,
+  },
+  {
+    id: 3,
+    title: "Joggers",
+    name: "joggers" as Category,
+  },
+  {
+    id: 4,
+    title: "Hoodies",
+    name: "hoodies" as Category,
+  },
+  {
+    id: 5,
+    title: "Shorts",
+    name: "shorts" as Category,
+  },
+];
