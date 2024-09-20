85fe3836686807d656d7466a688b5cad004424e4
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-09-20 13:50:11 +0530

    modified:   app/action.ts 	modified:   app/api/auth/creation/route.ts

--- app/action.ts
+++ app/action.ts
@@ -345,8 +345,8 @@ export async function CheckOut() {
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
       line_items: lineItems,
-      success_url: "https://retro-weebs.vercel.app/payment/success",
-      cancel_url: "https://retro-weebs.vercel.app/payment/cancel",
+      success_url: "https://www.retroweebs.com/payment/success",
+      cancel_url: "https://www.retroweebs.com/payment/cancel",
       metadata: {
         userId: user.id,
       },
