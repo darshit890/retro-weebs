a46cfe69cf4d5164a21e5152f9faf0bf1a4306a9
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-12-03 10:59:56 +0530

    final constants.ts

--- app/constants.ts
+++ app/constants.ts
@@ -4,5 +4,8 @@ export const adminUsers = [
     "contact@retroweebs.com",
     "adhyayan1999@outlook.com",
     "007adhyayan@gmail.com",
-    "berajoy04@gmail.com"
+    "berajoy04@gmail.com",
+    "yamanvohra300@gmail.com",         
+    "kk0262900@gmail.com",              
+    "meetme2046@gmail.com",
 ]
